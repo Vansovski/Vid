c15e19877cbe0cca2d10d7aea84a1e06b2f22593
Vansovski <marcelo_vansovski@hotmail.com>
2022-06-18 16:07:34 -0300

    MVC

---
+++ Models/Cliente.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vidly.Models
+{
+    public class Cliente
+    {
+        public int id { get; set; } 
+        
+        public string Name { get; set; }
+    }
+}
---
+++ ViewModels/MovieCustomers.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class MovieCustomers
+    {
+        public Movie Movie { get; set; }
+        public List<Cliente> Clientes { get; set; }
+    }
+}
